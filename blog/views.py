1a837411622c5151506093c9776909db5e7a8cbf
Benjie Edroso <61066771+benjieedroso@users.noreply.github.com>
2026-08-15 17:55:43 +0800

    create post model, add to admin

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-	{
-		'author': 'CoreyMS',
-		'title': 'Blog Post 1',
-		'content': 'First Post Content',
-		'date_posted': 'August 14 2026'
-	},
-		{
-		'author': 'Jane Doe',
-		'title': 'Blog Post 2',
-		'content': 'Second Post Content',
-		'date_posted': 'August 13 2026'
-	}
-]
+from .models import Post
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
