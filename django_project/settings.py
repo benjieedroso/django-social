ea9aee5d64e8cc6e0aeaf2536c80465cfad99db0
Benjie Edroso <61066771+benjieedroso@users.noreply.github.com>
2026-08-18 22:00:05 +0800

    add user profile, with signals

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Django Crispy Forms Bootstrap 5
 # https://github.com/django-crispy-forms/crispy-bootstrap5
